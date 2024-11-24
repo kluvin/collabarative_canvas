17ac66b970a7081a984d5f1726e9e8dfd832f5e3
Martin Kleiven <m.klei@live.no>
2024-11-24 08:30:00 +0100

    Add route for single cursor

--- lib/collabarative_canvas_web/router.ex
+++ lib/collabarative_canvas_web/router.ex
@@ -18,6 +18,7 @@ defmodule CollabarativeCanvasWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+    live "/canvas", Canvas
   end
 
   # Other scopes may use custom stacks.
