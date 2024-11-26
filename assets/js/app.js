451fcea4aa7c061ddee135f7fcb503e6a66630d9
Martin Kleiven <m.klei@live.no>
2024-11-26 10:29:53 +0100

    extract hooks

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,99 +21,18 @@ import "phoenix_html";
 import { Socket } from "phoenix";
 import { LiveSocket } from "phoenix_live_view";
 import topbar from "../vendor/topbar";
+import CanvasHook from './hooks/canvas'
 
 let csrfToken = document
   .querySelector("meta[name='csrf-token']")
   .getAttribute("content");
 
 
-let Hooks = {}
-
-Hooks.Canvas = {
-  mounted() {
-    const canvas = document.getElementById("canvas");
-    const ctx = canvas.getContext("2d");
-    ctx.lineWidth = 2;
-    ctx.lineCap = "round";
-    ctx.lineJoin = "round";
-    ctx.shadowBlur = 1;
-    ctx.shadowColor = "rgba(0, 0, 0, 0.5)";
-
-    let isDrawing = false;
-
-    const getRelativeCoords = (e) => ({
-      x: (e.pageX / window.innerWidth) * 100,
-      y: (e.pageY / window.innerHeight) * 100,
-    });
-
-    const getOffsetCoords = (e) => ({
-      x: e.offsetX,
-      y: e.offsetY,
-    });
-
-    const getCanvasCoords = ({ x, y }) => {
-      const rect = canvas.getBoundingClientRect();
-      const absX = (x / 100) * window.innerWidth;
-      const absY = (y / 100) * window.innerHeight;
-      return {
-        x: absX - rect.left,
-        y: absY - rect.top,
-      };
-    };
-
-    const startSegment = ({ x, y }, color) => {
-      ctx.strokeStyle = color;
-      ctx.beginPath();
-      ctx.moveTo(x, y);
-    };
-
-    const continueSegment = ({ x, y }, color) => {
-      ctx.strokeStyle = color;
-      ctx.lineTo(x, y);
-      ctx.stroke();
-    };
-
-    this.handleEvent("new-draw-segment", ({ newPath, color, x, y }) => {
-      const coords = getCanvasCoords({ x, y });
-      if (newPath) {
-        startSegment(coords, color);
-      } else {
-        continueSegment(coords, color);
-      }
-    });
-
-    canvas.addEventListener("mousedown", (e) => {
-      isDrawing = true;
-      startSegment(getOffsetCoords(e), this.el.dataset.userColor);
-      this.pushEvent("draw-segment", {
-        ...getRelativeCoords(e),
-        newPath: true,
-        color: this.el.dataset.userColor,
-      });
-    });
-
-    canvas.addEventListener("mousemove", (e) => {
-      if (isDrawing) {
-        continueSegment(getOffsetCoords(e), this.el.dataset.userColor);
-        this.pushEvent("draw-segment", {
-          ...getRelativeCoords(e),
-          newPath: false,
-          color: this.el.dataset.userColor,
-        });
-      }
-      this.pushEvent("mouse-move", { ...getRelativeCoords(e) });
-    });
-
-    canvas.addEventListener("mouseup", () => {
-      isDrawing = false;
-      this.pushEvent("mouse-up");
-    });
-  },
-};
-
 let liveSocket = new LiveSocket("/live", Socket, {
   longPollFallbackMs: 2500,
-  hooks: Hooks,
+  hooks: {
+    Canvas: CanvasHook
+  },
   params: { _csrf_token: csrfToken },
 });
 
