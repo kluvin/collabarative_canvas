f0471c39f2b2535937392e49ddc6fd88d2147b35
Martin Kleiven <m.klei@live.no>
2024-11-26 17:52:43 +0100

    Shift viewbox, slightly

--- lib/collabarative_canvas_web/live/canvas.ex
+++ lib/collabarative_canvas_web/live/canvas.ex
@@ -101,7 +101,7 @@ defmodule CollabarativeCanvasWeb.Canvas do
               height="25px"
               xmlns="http://www.w3.org/2000/svg"
               xmlns:xlink="http://www.w3.org/1999/xlink"
-              viewBox="0 0 21 21"
+              viewBox="8 3 21 21"
             >
               <polygon fill="black" points="8.2,20.9 8.2,4.9 19.8,16.5 13,16.5 12.6,16.6" />
               <polygon fill="currentColor" points="9.2,7.3 9.2,18.5 12.2,15.6 12.6,15.5 17.4,15.5" />
